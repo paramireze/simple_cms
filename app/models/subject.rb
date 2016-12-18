adb8df47292d2e57dc7bb072bb8269327d8d0d10
Paul Ramirez <paramireze@gmail.com>
2016-12-18 10:35:32 -0600

    learning lambda

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ApplicationRecord
+  scope :visibile, lambda { where(:visibile => true) }
+  scope :invisibile, lambda { where(:visibile => false) }
+  scope :sorted, lambda { order("position asc")}
+  scope :newest_first, lambda { order("created_at desc")}
+  scope :search, lambda {|query| where (["name like ?", "%#{query}%"])}
 end
