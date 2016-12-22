ed9718eebc019dbb8d6b006656111b7382811d41
Paul Ramirez <paramireze@gmail.com>
2016-12-22 10:03:42 -0600

    added page title to each page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,11 @@
 class Page < ApplicationRecord
 
-  belongs_to  :subject, { :optional => true }
+  belongs_to  :subject, { :optional => false }
   has_and_belongs_to_many :admin_users
+
+  scope :visibile, lambda { where(:visibile => true) }
+  scope :invisibile, lambda { where(:visibile => false) }
+  scope :sorted, lambda { order("position asc")}
+  scope :newest_first, lambda { order("created_at desc")}
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,12 @@
 class Section < ApplicationRecord
+  belongs_to  :page
   has_many    :section_edits
   has_many    :admin_users, :through => :section_edits
+
+  scope :visibile, lambda { where(:visibile => true) }
+  scope :invisibile, lambda { where(:visibile => false) }
+  scope :sorted, lambda { order("position asc")}
+  scope :newest_first, lambda { order("created_at desc")}
+  scope :search, lambda {|query| where (["name like ?", "%#{query}%"])}
+
 end
