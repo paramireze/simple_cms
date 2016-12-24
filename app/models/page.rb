3d3b012c95ad9eb8f585bfb7e0bb4079f1736ba0
Paul Ramirez <paramireze@gmail.com>
2016-12-24 10:35:39 -0600

    defined validation restraints to modal properties

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,4 +8,11 @@ class Page < ApplicationRecord
   scope :sorted, lambda { order("position asc")}
   scope :newest_first, lambda { order("created_at desc")}
 
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, within => 3..255
+  # use presence_of with length_of to disallow spaces
+  validates_uniqueness_of :permalink
+  # for unique values by subject us ":scope => subject_id"
 end
