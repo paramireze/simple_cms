acc6ddc4a517af221bea0db23c7890d49be67b2c
Paul Ramirez <paramireze@gmail.com>
2016-12-25 12:20:50 -0600

    created basic skeleton for login/admin pages/controller

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,16 @@
+class AccessController < ApplicationController
+  layout 'admin'
+  def menu
+    # displat text & links
+  end
+
+  def login
+    # login form
+  end
+
+  def attempt_login
+  end
+
+  def logout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   root  'demo#index'
 
+  #authentication actions
+  get  'admin', :to => 'access#menu'
+  get  'access/menu'
+  get  'access/login'
+  post 'access/attempt_login'
+  get  'access/logout'
+
+
   #gives all 8 actions on subject
   resources :subjects do
     member do
