e8505d3a6ce74131043d54c8d59450712f776a75
Paul Ramirez <paramireze@gmail.com>
2016-12-25 11:13:02 -0600

    used logger to write to log file

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,6 +5,7 @@ class SubjectsController < ApplicationController
   before_action :set_subject_count, :only => [:new, :create, :edit, :update]
 
   def index
+    logger.debug("*** Testing the logger. ***")
     @subjects = Subject.sorted
   end
 
