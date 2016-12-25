7c64511f2dc60d499ceae99bcc87565e847bde32
Paul Ramirez <paramireze@gmail.com>
2016-12-25 10:44:20 -0600

    added controller filters to populate variables that run before actions are called

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   layout 'admin'
+
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
   def index
     @pages = Page.sorted
   end
@@ -12,8 +15,6 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
     @page_count = Page.count + 1
-    @subjects = Subject.sorted
-
   end
 
   def create
@@ -24,26 +25,21 @@ class PagesController < ApplicationController
     else
       flash[:notice] = "failed to create"
       @page_count = Page.count + 1
-      @subjects = Subject.sorted
       render('new')
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count
-    @subjects = Subject.sorted
   end
 
   def update
     @page = Page.find(params[:id])
-    @subjects = Subject.sorted
     if @page.update_attributes(page_params)
       flash[:notice] = "hey, you updated a page. Well done matey!"
       redirect_to(pages_path)
     else
       flash[:notice] = "failed update"
-      @page_count = Page.count
       render('edit')
     end
   end
@@ -59,8 +55,20 @@ class PagesController < ApplicationController
     redirect_to(pages_path)
   end
 
+  private
+
   def page_params
     params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
   end
 
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_page_count
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @page_count += 1
+    end
+  end
 end
